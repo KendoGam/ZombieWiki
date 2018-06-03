cc113c8dfe79d6f955674e54149e0b2a6c16ce63
Jesus Manuel Valenzuela Soto <alivefractionz@outlook.com>
2018-06-03 16:42:13 -0500

    Zombiewiki subida 1

---
+++ app.js
@@ -0,0 +1,43 @@
+var http = require('http');
+var path = require('path');
+var express = require('express');
+var logger = require('morgan');
+var bodyParser = require('body-parser');
+
+var app = express();
+app.set('views',path.resolve(__dirname, 'views'));
+app.set('view engine', 'ejs');
+
+var publicPath = path.join(__dirname,'public');
+app.use('/recursos',express.static(publicPath));
+
+var entries =[];
+app.locals.entries = entries;
+
+app.use(logger('dev'));
+app.use(bodyParser.urlencoded({ extended: false}));
+
+app.get('/', (request,response) => response.render('index'));
+
+app.get('/victimas',(request, response)=> response.render('victimas'));
+app.get('/clases',(request, response)=> response.render('clases'));
+app.get('/armas',(request, response)=> response.render('armas'));
+app.get('/new-entry',(request, response)=> response.render('new-entry'));
+
+
+app.post('/new-entry', (request, response)=>{
+    if(!request.body.title || !request.body.body){
+        response.status(400).send('Error en la entradas')
+        return;
+    }
+    entries.push({
+        title: request.body.title,
+        body: request.body.body,
+        created: new Date()
+    });
+    response.redirect('/');
+});
+
+app.use((request, response)=> response.status(404).render('404'));
+
+http.createServer(app).listen(3000,()=> console.log('La aplicación ZombieWiki está corriendo en el puerto 3000'));
